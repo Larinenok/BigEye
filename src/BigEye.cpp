9886ef5d7f019310616f0f38b2ef62169f8797d8
6yntar05 <6yntar05@mail.ru>
2022-10-26 19:04:56 +0700

    Minor changes

--- src/BigEye.cpp
+++ src/BigEye.cpp
@@ -1,18 +1,28 @@
 #include "engine/engine.hpp"
 #include "input/stream.hpp"
 #include "ui/window.hpp"
+#include "ui/feedback.hpp"
+#include "excepts.hpp"
 
 int main(int argc, char *argv[])
 {
-    // OpenCV Build Test
+    // OpenCV Test
     cv::VideoCapture streamVideo = input::openVideo("./video.webm");
     cv::VideoCapture streamCam = input::openCamera(0);
 
     streamVideo.release();
     streamCam.release();
 
-    // QT Build Test
-    QCoreApplication a(argc, argv);
+    // ui/feedback test (check taskpool pls)
+    ui::error("aboba");
+    ui::warn("aboba");
+    ui::info("aboba");
+    ui::msg("aboba");
 
+    // excepts test
+    throw excepts::error("msg","file","function","info");
+
+    // QT Test
+    QCoreApplication a(argc, argv);
     return a.exec();
 }
