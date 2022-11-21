eb11f805c840a7ffa11b0a1dd5d0f0000ddb4a90
6yntar05 <6yntar05@mail.ru>
2022-11-21 22:40:37 +0700

    Added DryRun mode

--- src/BigEye.cpp
+++ src/BigEye.cpp
@@ -11,20 +11,25 @@
 
 // Runtime defaults
 bool runtime::FLAG_headless = false;
+bool runtime::FLAG_dryRun   = false;
 
 int main(int argc, char *argv[]) {
     utils::parseArgs(argc, argv);  // this function can change runtime:: flags!
 
     // Connect to database
-    db::db database{db::backends::postgres,
-                    "postgres",
-                    "youmu",
-                    "bigeye",
-                    {"127.0.0.1", db::getDefaults(db::backends::postgres).addr.port}};
+    try {
+        db::db database{db::backends::postgres,
+                        "postgres",
+                        "youmu",
+                        "bigeye",
+                        {"127.0.0.1", db::getDefaults(db::backends::postgres).addr.port}};
 
-    database.stats();
+        database.stats();
+    } catch (std::exception &e) {
+        ui::error("Something goes whong while BigEye trying to access database! Using DryRun mode!");
+        runtime::FLAG_dryRun = true;
+    }
 
-    /*
     cv::Mat frame;
     int deviceID = 0;
 
@@ -39,5 +44,5 @@ int main(int argc, char *argv[]) {
 
         imshow("Live", engine::face_detection(frame, faceCascade));
         if (cv::waitKey(5) == 27) break; // ESC
-    }*/
+    }
 }
